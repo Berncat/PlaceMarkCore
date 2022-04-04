3d1ff33b711ab4eb2ded6f99a3e17bc95f435b9b
Bernard Cattigan <77303547+Berncat@users.noreply.github.com>
2022-04-05 00:33:41 +0100

    Update server.js - Heroku

--- src/server.js
+++ src/server.js
@@ -32,8 +32,7 @@ const swaggerOptions = {
 
 async function init() {
   const server = Hapi.server({
-    port: 3000,
-    host: "localhost",
+    port: process.env.PORT || 3000,
   });
 
   await server.register(Vision);
